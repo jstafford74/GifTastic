607074aecca6c194ff8ec809e0e92da4f743ea2e
Jeremiah Stafford <jstafford@slmdevice.com>
2019-09-17 23:55:49 -0400

    clean but broken code

--- assets/javascript/Giphy.js
+++ assets/javascript/Giphy.js
@@ -1,88 +1,45 @@
 
-//---------------Initialize Firebase (YOUR OWN APP)------------------------
-var firebaseConfig = {
-    apiKey: "AIzaSyAlRsXALoi52VoaIuSpgtxUsWV3w-l6BPY",
-    authDomain: "classproj-c95fe.firebaseapp.com",
-    databaseURL: "https://classproj-c95fe.firebaseio.com",
-    projectId: "classproj-c95fe",
-    storageBucket: "",
-    messagingSenderId: "813898612157",
-    appId: "1:813898612157:web:e01fb2f037bebac48d26d4"
-};
-
-//----------------Initialize Firebase-------------------------------
-firebase.initializeApp(firebaseConfig);
-
 //----------------Set Variables--------------------
-var database = firebase.database();
-var storageRef = firebase.database().ref('b/');
-var actTop = firebase.database().ref('b/activeTopic');
-var initialValue = 0;
 var gifApiKey = "pKiLNKQV2SjERLUPH7vMCwMB42eoCBYN";
 var topics = ["Conan Obrien", "Awkward", "Excited", "Drunk", "Mind Blown", "Halloween", "Tom Brady"];
 var gifLimit = 10;
 var gifRating = "r";
 
 
-// if ($("#topic-input").empty() == true) {
-//     console.log("true");
-database.ref('b/').on("value", function (snapshot) {
-    topic = snapshot.val().activeTopic;
-});
-// }
-// else {
-//     topic = $("#topic-input").val().trim();
-// }
 
-
-// Use the below variable clickCounter to keep track of the clicks.
-var clickCounter = initialValue;
-
-renderButtons(topics);
-
-
-// Set Database Notation--------------------------------------------------------------
-function write2DB(clickCounter, topics, topic) {
-    database.ref('b/').set({
-        clickCount: clickCounter,
-        topics: topics,
-        activeTopic: topics[4]
-    });
-}
-
-// Initialize Display-----------------------------------------------------------------
-// var topic = database.ref('b/activeTopic');
-// var gifURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + gifApiKey + "&limit=" + gifLimit;
-// $.ajax({
-//     url: gifURL,
-//     method: "GET"
-// }).then(function (response) {
-//     var results = response.data;
-//     renderCard(results);
-// });
-
+//-------Add Gif Button--------------------------
 $("#add-gif").on("click", function (event) {
-    // console.log($(this));
-
     event.preventDefault();
     var topic = $("#topic-input").val().trim();
     topics.push(topic);
-
-    database.ref('b/').on("value", function (snapshot) {
-        topic = snapshot.val().activeTopic;
-    });
-    console.log(topic);
+    var gifRating = $("#rating")[0].value;
+    var gifLimit = $("#limit")[0].value;
     var gifURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + gifApiKey + "&limit=" + gifLimit;
-    clickCounter++;
-    write2DB(clickCounter, topics, topic);
+    src = [];
+    stillURL = [];
+    animeURL = [];
+    title = [];
+    rating = [];
     $.ajax({
         url: gifURL,
         method: "GET"
     }).then(function (response) {
-        var results = response.data;
+        results = response.data;
+        renderCard(results);
+    });
+});
+
+
+function renderCard(results) {
+    for (var i = 0; i < gifLimit; i++) {
+        src.push(results[i].images.fixed_height_still.url);
+        stillURL.push(results[i].images.fixed_height_still.url);
+        animeURL.push(results[i].images.fixed_height.url);
+        title.push(results[i].title.toUpperCase());
+        rating.push(results[i].rating.toUpperCase());
         var gifCard = $("<div>");
         gifCard.addClass('card border-success mb-3');
-        gifCard.attr('style', 'max-width: 18rem;max-height: 24rem');
+        gifCard.attr('style', 'max-width: 24rem;max-height: 20rem');
         var gifImg = $("<img>");
         gifImg.addClass("card-img-top gif");
         gifImg.attr("data-state", "still");
@@ -92,76 +49,27 @@ $("#add-gif").on("click", function (event) {
         h.addClass('card-title');
         var p = $("<p>");
         p.addClass("card-text");
-        for (var i = 0; i < gifLimit; i++) {
-            var src = results[i].images.fixed_height_still.url;
-            var stillURL = results[i].images.fixed_height_still.url;
-            var animeURL = results[i].images.fixed_height.url;
-            var title = results[i].title.toUpperCase();
-            var rating = results[i].rating.toUpperCase();
-            gifImg.attr("src", src[i]);
-            gifImg.attr("data-still", stillURL[i]);
-            gifImg.attr("data-animate", animeURL[i]);
-            h.text("Title: " + title[i]);
-            p.text("Rating: " + rating[i]);
-            cardText.append(h, p);
-            gifCard.append(gifImg, cardText);
-            $('#gifResults').prepend(gifCard);
+        gifImg.attr("src", src[i]);
+        gifImg.attr("data-still", stillURL[i]);
+        gifImg.attr("data-animate", animeURL[i]);
+        h.text("Title: " + title[i]);
+        p.text("Rating: " + rating[i]);
+        cardText.append(h, p);
+        gifCard.append(gifImg, cardText);
+        $('#gifResults').prepend(gifCard);
 
-        }
-
-
-    });
-    renderButtons(topics);
-});
-
-
-
-function renderCard(gifURL) {
-    // $.ajax({
-    //     url: gifURL,
-    //     method: "GET"
-    // }).then(function (response) {
-    //     var results         = response.data;
-    //     var gifCard         = $("<div>");
-    //         gifCard.addClass('card border-success mb-3');
-    //         gifCard.attr('style', 'max-width: 18rem;max-height: 24rem');
-    //     var gifImg          = $("<img>");
-    //         gifImg.addClass("card-img-top gif");
-    //         gifImg.attr("data-state", "still");
-    //     var cardText        = $("<div>");
-    //     cardText.addClass("card-body");
-    //     var h               = $("<h5>");
-    //         h.addClass('card-title');
-    //     var p               = $("<p>");
-    //         p.addClass("card-text");
-    //     for (var i = 0; i < gifLimit; i++) {
-    //         var src             = results[i].images.fixed_height_still.url;
-    //         var stillURL        = results[i].images.fixed_height_still.url;
-    //         var animeURL        = results[i].images.fixed_height.url;
-    //         var title           = results[i].title.toUpperCase();
-    //         var rating          = results[i].rating.toUpperCase();
-    //         gifImg.attr("src", src[i]);
-    //         gifImg.attr("data-still", stillURL[i]);
-    //         gifImg.attr("data-animate", animeURL[i]);
-    //         h.text("Title: " + title[i]);
-    //         p.text("Rating: " + rating[i]);
-    //         cardText.append(h, p);
-    //         gifCard.append(gifImg, cardText);
-    //         $('#gifResults').prepend(gifCard);
-
-    //     }
-
-
-    // });
+    }
 
 }
 
+
+
+//----------Render Buttons Function----------------
 function renderButtons(topics) {
     $("#button-view").empty();
     for (var i = 0; i < topics.length; i++) {
         var topic = topics[i].replace(" ", "");
         var gifURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + gifApiKey + "&limit=" + gifLimit;
-
         var gifLi = $('<li>');
         var gifBut = $('<button>');
 
@@ -179,61 +87,26 @@ function renderButtons(topics) {
 }
 
 
-
+//---------------Any Button Click Function--------------------
 $("button[name='viewGIF']").click(function () {
     var gifURL = $(this)[0].attributes.formaction.value;
-    clickCounter++;
-    write2DB(clickCounter, topics, topic);
+   console.log($(this));
     src = [];
     stillURL = [];
     animeURL = [];
     title = [];
     rating = [];
-
     $.ajax({
         url: gifURL,
         method: "GET"
     }).then(function (response) {
         var results = response.data;
-       
-
-        for (var i = 0; i < gifLimit; i++) {
-            src.push(results[i].images.fixed_height_still.url);
-            stillURL.push(results[i].images.fixed_height_still.url);
-            animeURL.push(results[i].images.fixed_height.url);
-            title.push(results[i].title.toUpperCase());
-            rating.push(results[i].rating.toUpperCase());
-            var gifCard = $("<div>");
-            gifCard.addClass('card border-success mb-3');
-            gifCard.attr('style', 'max-width: 18rem;max-height: 24rem');
-            var gifImg = $("<img>");
-            gifImg.addClass("card-img-top gif");
-            gifImg.attr("data-state", "still");
-            var cardText = $("<div>");
-            cardText.addClass("card-body");
-            var h = $("<h5>");
-            h.addClass('card-title');
-            var p = $("<p>");
-            p.addClass("card-text");
-            gifImg.attr("src", src[i]);
-            gifImg.attr("data-still", stillURL[i]);
-            gifImg.attr("data-animate", animeURL[i]);
-            h.text("Title: " + title[i]);
-            p.text("Rating: " + rating[i]);
-            cardText.append(h, p);
-            gifCard.append(gifImg, cardText);
-            $('#gifResults').prepend(gifCard);
-
-        }
-
-
+        renderCard(results);
     });
+
 });
 
-
-
-
-
+//---------Initialize and Change State------------------
 $(document).on("click", ".gif", function () {
     var state = $(this).attr("data-state");
     if (state === "still") {
@@ -245,7 +118,7 @@ $(document).on("click", ".gif", function () {
     }
 });
 
-
+renderButtons(topics);
 
 
 
